e787fb0d5942f2f206e2398a4bba41511152d41a
Harsh Raj <officialharshrajcse@gmail.com>
2025-06-14 11:28:12 +0000

    not use

--- src/pages/QuizPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AddPlayerForm from '../components/AddPlayerForm';
-
-const QuizPage: React.FC = () => {
-  return <AddPlayerForm />;
-};
-
-export default QuizPage;
